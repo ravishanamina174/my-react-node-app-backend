4faca322eab3e7fe67c8ca4b05cb43f43cb0dacf
Raveesha <ravishanamina174@gmail.com>
2025-06-16 22:51:15 +0530

    Initial commit

---
+++ src/api/middleware/authentication-middleware.js
@@ -0,0 +1,13 @@
+import UnauthorizedError from "../../domain/errors/unauthorized-error.js";
+
+const isAuthenticated = (req, res, next) => {
+  const isUserLoggedIn = false;
+  if (!isUserLoggedIn) {
+    throw new UnauthorizedError("Unauthorized");
+  } else {
+    next();
+  }
+};
+
+export default isAuthenticated;
+
